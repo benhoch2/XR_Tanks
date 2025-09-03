04b5bacdcda9a0c8c6a42edd92009e7051e82acf
benhoch2 <45126030+benhoch2@users.noreply.github.com>
2025-09-03 21:56:49 +0300

    new config UI

--- Assets/ConfigManager.cs
+++ Assets/ConfigManager.cs
@@ -5,22 +5,38 @@ using UnityEngine.SceneManagement; // <-- added
 
 public class ConfigManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject configCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("ConfigManager started");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // Public method to reload the currently active scene
     public void ReloadScene()
     {
+        Debug.Log("Reloading scene...");
         Scene active = SceneManager.GetActiveScene();
         SceneManager.LoadScene(active.name);
     }
+
+    public void CancelButton()
+    {
+        Debug.Log("Cancel button pressed");
+        configCanvas.SetActive(false);
+    }
+
+    public void SaveButton()
+    {
+        Debug.Log("Save button pressed");
+
+    }   
 }
